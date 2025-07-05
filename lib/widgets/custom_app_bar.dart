d013fb1e6e84dda1994fabd7d8a16f8def121498
mjfrshddq <2206060@itg.ac.id>
2025-07-06 00:00:38 +0700

    add custom app bar widget

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<Widget>? actions;
+  final Widget? leading;
+  final bool centerTitle;
+  final Color? backgroundColor;
+  final double elevation;
+  final PreferredSizeWidget? bottom;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.actions,
+    this.leading,
+    this.centerTitle = false,
+    this.backgroundColor,
+    this.elevation = 0,
+    this.bottom,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 18.sp,
+          fontWeight: FontWeight.w600,
+          color: Colors.grey.shade800,
+        ),
+      ),
+      actions: actions,
+      leading: leading,
+      centerTitle: centerTitle,
+      backgroundColor: backgroundColor ?? Colors.white,
+      surfaceTintColor: Colors.white,
+      elevation: elevation,
+      scrolledUnderElevation: 1,
+      shadowColor: Colors.black.withOpacity(0.1),
+      bottom: bottom,
+    );
+  }
+
+  @override
+  Size get preferredSize =>
+      Size.fromHeight(kToolbarHeight + (bottom?.preferredSize.height ?? 0));
+}
